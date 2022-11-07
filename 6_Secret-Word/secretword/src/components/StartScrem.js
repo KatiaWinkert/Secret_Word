5d2e4618b2dbd9aaa092766ff3cb2efa1ab7dd0f
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2022-11-07 00:14:53 -0300

    commit
    Atualização

---
+++ 6_Secret-Word/secretword/src/components/StartScrem.js
@@ -0,0 +1,13 @@
+import './StartScrem.css'
+
+const StartScrem = () => {
+  return (
+    <div className="start">
+        <h1>Secret Word</h1>
+        <p>clique no botão a baixo para jogar!</p>
+        <button>Começar o jogo:</button>
+        </div>
+  )
+}
+
+export default StartScrem
