558b671c83e7f14933420b02f56e037ee242ce38
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2022-11-07 21:59:10 -0300

    Game.js
    Atualização

--- 6_Secret-Word/secretword/src/components/Game.js
+++ 6_Secret-Word/secretword/src/components/Game.js
@@ -1,33 +1,52 @@
 import './Game.css'
 
-const Game = ({ verifyLetter }) => {
+const Game = ({
+  verifyLetter,
+  pickedWord,
+  letters,
+  pickedCategory,
+  guessedLetters,
+  wrongletters,
+  guesses,
+  score,
+}) => {
   return (
     <div className="game">
       <p className="points">
-        <span>Pontuação: 000</span>
+        <span>Pontuação: {score}</span>
       </p>
       <h1>Adivinhe a palavra:</h1>
       <h3 className="tip">
-        Dica sobre a palavra: <span>Dica....</span>
+        Dica sobre a palavra: <span>{pickedCategory}</span>
       </h3>
+      <p>Você ainda tem {guesses} tentativas(s).</p>
       <div className="wordContainer">
-        <span className='letter'>A</span>
-        <span className='blankSquare'></span>
- </div>
- <div className='letterContainer'>
-  <p>Adivinhe as letras da palavra:</p>
-  <form >
-    <input type="text" name='letter' maxLength='1' required />
-    <button>Jogar!</button>
-  </form>
- </div>
- <div className="wronglettersContainer">
-  <p>Letras já jogadas: </p>
-  <span>a,</span>
-  <span>b,</span>
- </div>
+        {letters.map((letter, i) =>
+          guessedLetters.includes(letter) ? (
+            <span key={i} className="letter">{letter}</span>
+          ) : (
+            <span key={i} className="blankSquare"></span>
+          )
+        )}
+      </div>
+      <div className="letterContainer">
+        <p>Adivinhe as letras da palavra:</p>
+        <form>
+          <input type="text" name="letter" maxLength="1" required />
+          <button>Jogar!</button>
+        </form>
+      </div>
+      <div className="wronglettersContainer">
+        <p>Letras já jogadas: </p>
+        {wrongletters.map((letter, i) => (
+          <span key={i}>{letter}, </span>
+        ))}
+      </div>
     </div>
   )
 }
 
 export default Game
+
+  //<span className="letter">A</span>
+       // <span className="blankSquare"></span>//
