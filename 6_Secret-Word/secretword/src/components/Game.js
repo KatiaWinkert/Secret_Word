6d7b701e942ce91b21499248b7a7d9aa37795ab6
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2022-11-08 22:57:03 -0300

    Game.js
    Atualização

--- 6_Secret-Word/secretword/src/components/Game.js
+++ 6_Secret-Word/secretword/src/components/Game.js
@@ -1,4 +1,5 @@
 import './Game.css'
+import { useState, useRef } from 'react'
 
 const Game = ({
   verifyLetter,
@@ -10,6 +11,17 @@ const Game = ({
   guesses,
   score,
 }) => {
+  const [letter, setLetter] = useState('')
+  const letterInputRef = useRef(null)
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    verifyLetter(letter)
+    setLetter('')
+    letterInputRef.current.focus()
+  }
+
   return (
     <div className="game">
       <p className="points">
@@ -23,7 +35,9 @@ const Game = ({
       <div className="wordContainer">
         {letters.map((letter, i) =>
           guessedLetters.includes(letter) ? (
-            <span key={i} className="letter">{letter}</span>
+            <span key={i} className="letter">
+              {letter}
+            </span>
           ) : (
             <span key={i} className="blankSquare"></span>
           )
@@ -31,8 +45,16 @@ const Game = ({
       </div>
       <div className="letterContainer">
         <p>Adivinhe as letras da palavra:</p>
-        <form>
-          <input type="text" name="letter" maxLength="1" required />
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            name="letter"
+            maxLength="1"
+            required
+            onChange={(e) => setLetter(e.target.value)}
+            value={letter}
+            ref={letterInputRef}
+          />
           <button>Jogar!</button>
         </form>
       </div>
@@ -48,5 +70,5 @@ const Game = ({
 
 export default Game
 
-  //<span className="letter">A</span>
-       // <span className="blankSquare"></span>//
+//<span className="letter">A</span>
+// <span className="blankSquare"></span>//
